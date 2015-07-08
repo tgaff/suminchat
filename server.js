ccbe4284dfa392e5aa27e7dba4970bcc30e5067d
tgaff <tgaff@alumni.nd.edu>
2015-07-08 12:34:42 -0700

    create a root route to redirect to chat.html

--- server.js
+++ server.js
@@ -59,6 +59,18 @@ function parseTokenResponseData(chunk) {
   console.log(data);
   return data;
 }
+
+function redirectToChat(clientReq, res, next) {
+  console.log(clientReq.url);
+  if (clientReq.url == '/') {
+    res.writeHead(301, {Location: '/chat.html'});
+    res.end();
+  } else {
+    next();
+  };
+}
 var port = (process.env.PORT || 8080);
 app.use('/translatorkey', getTranslatorKey);
+app.use('/', redirectToChat);
+
 app.use(serveStatic(__dirname)).listen(port);
